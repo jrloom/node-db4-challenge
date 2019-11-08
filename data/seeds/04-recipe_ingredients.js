7567f5a4626831790fa03d812432674b5867fefa
Jason Loomis <jason@jrloomis.com>
2019-11-07 22:22:09 -0500

    added measurements

--- data/seeds/04-recipe_ingredients.js
+++ data/seeds/04-recipe_ingredients.js
@@ -3,12 +3,42 @@ exports.seed = function(knex) {
     .truncate()
     .then(function() {
       return knex("recipe_ingredients").insert([
-        { recipe_id: 1, ingredient_id: 1 },
-        { recipe_id: 1, ingredient_id: 2 },
-        { recipe_id: 2, ingredient_id: 1 },
-        { recipe_id: 2, ingredient_id: 3 },
-        { recipe_id: 3, ingredient_id: 1 },
-        { recipe_id: 3, ingredient_id: 4 }
+        {
+          recipe_id: 1,
+          ingredient_id: 1,
+          quantity: 1,
+          measurement: "guesstimate"
+        },
+        {
+          recipe_id: 1,
+          ingredient_id: 2,
+          quantity: 1,
+          measurement: "guesstimate"
+        },
+        {
+          recipe_id: 2,
+          ingredient_id: 1,
+          quantity: 1,
+          measurement: "guesstimate"
+        },
+        {
+          recipe_id: 2,
+          ingredient_id: 3,
+          quantity: 1,
+          measurement: "guesstimate"
+        },
+        {
+          recipe_id: 3,
+          ingredient_id: 1,
+          quantity: 1,
+          measurement: "guesstimate"
+        },
+        {
+          recipe_id: 3,
+          ingredient_id: 4,
+          quantity: 1,
+          measurement: "guesstimate"
+        }
       ]);
     });
 };
