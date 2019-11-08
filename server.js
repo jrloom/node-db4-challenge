028057b20f53516bbff13ca878b594d6127c34cf
Jason Loomis <jason@jrloomis.com>
2019-11-07 19:37:36 -0500

    server running

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const recipeRouter = require("./routes/recipeRouter");
+
+const server = express;
+
+server.use(express.json());
+server.use("/api/recipes", recipeRouter);
+
+// * sanity
+server.get("/", (req, res) =>
+  res.status(200).json({ message: "Good news everyone" })
+);
+
+module.exports = server;
